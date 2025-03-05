952ae8c46c8932ab30ce6f913009b0e323b6f13f
yudai2929 <yudai201729@icloud.com>
2025-03-05 12:48:24 +0900

    feat: create task

--- pkg/adapter/handler/task.go
+++ pkg/adapter/handler/task.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	api "github.com/yudai2929/task-app/doc/gen"
+	"github.com/yudai2929/task-app/pkg/usecase"
 )
 
 func (h *Handler) GetTask(ctx context.Context, params api.GetTaskParams) (*api.Task, error) {
@@ -15,7 +16,32 @@ func (h *Handler) ListTasks(ctx context.Context) ([]api.Task, error) {
 }
 
 func (h *Handler) CreateTask(ctx context.Context, req *api.CreateTaskReq) (*api.Task, error) {
-	return nil, nil
+	userID := UserIDFromContext(ctx)
+
+	dueDate := req.GetDueDate().Value
+	in := &usecase.CreateTaskInput{
+		UserID:      userID,
+		Title:       req.GetTitle(),
+		Description: req.GetDescription(),
+		DueDate:     &dueDate,
+	}
+	out, err := h.tu.CreateTask(ctx, in)
+	if err != nil {
+		return nil, err
+	}
+
+	dueDateResp := api.NewOptDateTime(*out.Task.DueDate)
+	if out.Task.DueDate == nil {
+		dueDateResp.Reset()
+	}
+	return &api.Task{
+		ID:          out.Task.ID,
+		UserID:      out.Task.UserID,
+		Title:       out.Task.Title,
+		Description: out.Task.Description,
+		Status:      out.Task.Status.Int(),
+		DueDate:     dueDateResp,
+	}, nil
 }
 
 func (h *Handler) UpdateTask(ctx context.Context, req *api.UpdateTaskReq, params api.UpdateTaskParams) (*api.Task, error) {
